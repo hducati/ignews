c008921e743a3322545746dafc4e62f2fd6f7f18
Henrique Ducati <henrique.dcti@gmail.com>
2022-05-26 09:16:40 -0300

    feat: add faunadb and nextauth methods

--- src/components/SignInButton/SignInButton.tsx
+++ src/components/SignInButton/SignInButton.tsx
@@ -1,24 +1,27 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, useSession, signOut } from 'next-auth/react'
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const session  = useSession();
 
-  return isUserLoggedIn ? (
+  return session.status === 'authenticated' ? (
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d361" />
-      Henrique Ducati
+      {session.data.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417" />
       Sign in with GitHub
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,47 @@
+import { query as q } from 'faunadb';
+import NextAuth from 'next-auth';
+import GithubProvider from 'next-auth/providers/github'
+
+import { fauna } from '../../../services/fauna';
+
+export default NextAuth({
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async signIn({ user }) {
+      const email = user.email
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(email)
+              )
+            )
+          )
+        )
+        return true;
+      } catch {
+        return false;
+      }
+    }
+  }
+})
